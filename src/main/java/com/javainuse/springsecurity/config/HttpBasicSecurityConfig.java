9147291fa0263a7f70ffca156bbe03664a0e0e27
Jaeyoung Lim <limjyjustin@gmail.com>
2024-01-18 15:01:14 +0900

    feat: 권한에 따른 Spring Security 설정 추가

---
+++ src/main/java/com/javainuse/springsecurity/config/HttpBasicSecurityConfig.java
@@ -0,0 +1,49 @@
+package com.javainuse.springsecurity.config;
+
+import lombok.RequiredArgsConstructor;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.authentication.dao.DaoAuthenticationProvider;
+import org.springframework.security.config.Customizer;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.config.annotation.web.configurers.AbstractHttpConfigurer;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+import org.springframework.security.web.SecurityFilterChain;
+
+@RequiredArgsConstructor
+@EnableWebSecurity
+@Configuration
+public class HttpBasicSecurityConfig {
+
+    private final CustomUserDetailsService userDetailsService;
+
+    @Bean
+    public SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception {
+        http
+                .csrf(AbstractHttpConfigurer::disable)
+                .authorizeHttpRequests((authorizeRequests) ->
+                        authorizeRequests
+                                .requestMatchers("helloadmin").hasRole("ADMIN")
+                                .requestMatchers("hellouser").hasAnyRole("ADMIN", "USER")
+                )
+                .httpBasic(Customizer.withDefaults());
+
+        return http.build();
+    }
+
+    @Bean
+    public DaoAuthenticationProvider daoAuthenticationProvider() throws Exception {
+        DaoAuthenticationProvider daoAuthenticationProvider = new DaoAuthenticationProvider();
+
+        daoAuthenticationProvider.setUserDetailsService(userDetailsService);
+        daoAuthenticationProvider.setPasswordEncoder(bCryptPasswordEncoder());
+
+        return daoAuthenticationProvider;
+    }
+
+    @Bean
+    public static BCryptPasswordEncoder bCryptPasswordEncoder() {
+        return new BCryptPasswordEncoder();
+    }
+}
---
+++ src/main/java/com/javainuse/springsecurity/controller/ResourceController.java
@@ -0,0 +1,18 @@
+package com.javainuse.springsecurity.controller;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class ResourceController {
+
+    @GetMapping({"/hellouser"})
+    public String helloUser(){
+        return "Hello User";
+    }
+
+    @GetMapping({"/helloadmin"})
+    public String helloAdmin(){
+        return "Hello Admin";
+    }
+}
